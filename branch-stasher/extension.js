0f03ed898a3d60b25283deefee468fee2ceae860
ssanthoshss <ssanthoshss98@gmail.com>
2023-04-15 22:20:18 +0530

    Added restore tabs functionality and fixed onchange branch

--- branch-stasher/extension.js
+++ branch-stasher/extension.js
@@ -1,41 +1,48 @@
 const vscode = require('vscode');
 
 function activate(context) {
-	console.log('Congratulations, your extension "branch-stasher" is now active!');
+	console.log('Started');
 
-	const configData = {}
+	const branchData = {}
 	const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports
 	const gitRepository = gitExtension?.getAPI(1).repositories[0].repository
-	const currentBranch = gitRepository.HEAD.name
 
-	const saveTabs = () => {
+	const saveTabs = (currentBranch) => {
 		const tabs = vscode.window.tabGroups.all.flatMap(({ tabs }) => tabs.map(tab => (
 			{
 				path: tab.input.uri.path,
 				viewColumn: tab.group.viewColumn
 			}
 		)))
-		configData[currentBranch] = tabs
-		return JSON.stringify(configData)
+		branchData[currentBranch] = tabs
+		return JSON.stringify(branchData)
+	}
+
+	const restoreTabs = (currentBranch) => {
+		if(currentBranch in branchData){
+			const tabs = branchData[currentBranch]
+			tabs.forEach(async(tab) => {
+				console.log(tab.path)
+				await vscode.window.showTextDocument(
+					vscode.Uri.file(tab.path),
+					{
+						preview: false,
+						viewColumn: tab.viewColumn
+					}
+				)
+			})
+		}
 	}
 
 	gitRepository.onDidChangeOperations((e) => {
-		console.log(e)
-
-		if(e==="Checkout"){
-			console.log(`branch changed`);
-			saveTabs()
+		if(e === 'Checkout'){
+			saveTabs(gitRepository.HEAD.name)
+		}
+		if(e.operation?.kind === 'Checkout'){
+			restoreTabs(e.operation?.refLabel)
 		}
 	});
 
-	// const restoreTabs = async () => {
-	// 	vscode.window.tabGroups.all.flatMap(({ tabs }) => tabs.forEach(async(tab) => {
-	// 		await vscode.window.showTextDocument(tab.input.uri.path)
-	// 		console.log(tab.input.uri.path)
-	// 		console.log(tab.group.viewColumn)
-	// 	}))
-	// 	// await vscode.window.showTextDocument(await vscode.window.tabGroups.all[0].tabs[0].input.uri.path, {preview: false, viewColumn: 2})
-	// }
 	let disposable = vscode.commands.registerCommand('branch-stasher.start', function () {
 		if (gitExtension?.getAPI(1).repositories.length < 0) {
 			vscode.window.showErrorMessage('Unable to find Repo. Please add Repo to your current or parent directory');
